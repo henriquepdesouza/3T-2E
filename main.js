4ddf6b8b8369df4704a779b1b38bbe5816b932db
henriquepdesouza <160139309+henriquepdesouza@users.noreply.github.com>
2024-10-28 08:52:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
 function criarCartao(categoria,pergunta,resposta){
-    console.log(categoria,pergunta,resposta)
+    let container = document.getElementById('container')
+    let article = document.createElement('article')
+    article.className = 'cartao'
+    article.innerHTML = 'Isso é um teste'
+    container.appendChild(article)
 }
+
 criarCartao('Biologia','Qual a funçao da mitocondria','Produçao de energia')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
