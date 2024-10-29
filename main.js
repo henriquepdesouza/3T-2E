2a91aa6b8dd3af0172595de1a85d0c8b60c4e102
henriquepdesouza <160139309+henriquepdesouza@users.noreply.github.com>
2024-10-29 08:52:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ function criarCartao(categoria,pergunta,resposta){
     let container = document.getElementById('container')
     let article = document.createElement('article')
     article.className = 'cartao'
-    article.innerHTML = 'Isso é um teste'
+    article.innerHTML = `
+    <div class="cartao-conteudo">
+        <h3> ${categoria}</h3>
+    <div class="cartao-conteudo-pergunta"</div>
+        <p> ${pergunta} </p>> 
+    <div class="cartao-conteudo-resposta">
+        <p> ${resposta} </p>
+    </div>
+    </div>
+    `
     container.appendChild(article)
 }
 
